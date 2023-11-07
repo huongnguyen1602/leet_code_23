34c4e2529846b98dc8ed4ceaf93eac5ea66e6434
huongnguyen1602 <nguyenhuuhuong1602@gmail.com>
2023-11-07 21:47:01 +0700

    update

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ fn main() {
     // let mut b: Vec<u8> = a.as_bytes().into();
     // b.dedup();
     // println!("{:?}",b);
-    // test 4
+    // test 5
 
     let s = String::from("abcabcbb");
     println!("{}",length_of_longest_substring(s));
